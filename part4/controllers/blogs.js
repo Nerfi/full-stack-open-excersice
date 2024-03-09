a642f418e1d9190a5bde5d6fb86e6817919ef413
juan <juanchuu.jfpc@gmail.com>
2024-03-09 15:03:11 +0100

    working as expected in 4.2

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,10 +1,22 @@
-const blogRouter = require('express').Router();
-
+const blogRouter = require("express").Router();
 const Blog = require("../models/blogModel");
+//app.use("/api/blogs", blogRoutes); esta linea ki que hace es poner /api/blogs como prefijo de cualquiera
+// de nuestras rutas
 
 blogRouter.get("/", (req, res) => {
-    Blog.find({}).then(response => res.json(response))
-  
-})
+  Blog.find({})
+    .then((response) => res.json(response))
+    .catch((err) => console.log(err));
+});
+
+blogRouter.post("/", (req, res) => {
+  const blog = new Blog(req.body);
+  blog
+    .save()
+    .then((blogCreated) => {
+      res.status(201).json(blogCreated);
+    })
+    .catch((err) => console.log(err));
+});
 
 module.exports = blogRouter;
