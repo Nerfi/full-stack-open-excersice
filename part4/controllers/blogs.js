29abe6ff81b7f8e293a2f5d73944ddcc93330713
juan <juanchuu.jfpc@gmail.com>
2024-03-16 15:46:07 +0100

    created function in order to extract the token send by the user in header,not in used yet, exercise 4.19 not done yet, needs to be finish

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,10 +1,21 @@
 const blogRouter = require("express").Router();
 const Blog = require("../models/blogModel");
 const User = require("../models/userModel");
+const jwt = require('jsonwebtoken');
 //testing
 const blogList = require("../utils/list_helper");
 //app.use("/api/blogs", blogRoutes); esta linea ki que hace es poner /api/blogs como prefijo de cualquiera
 // de nuestras rutas
+
+//helper function in order to extract the token 
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.startsWith("Bearer ")) {
+    return authorization.replace("Bearer ", "");
+  }
+
+  return null;
+};
 
 blogRouter.get("/", async (req, res, next) => {
   try {
