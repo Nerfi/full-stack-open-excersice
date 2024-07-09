8eb2c51094254ce84c5ab83a22a9d2b96d1d8112
juan <juanchuu.jfpc@gmail.com>
2024-07-09 13:26:29 +0200

    added singleUser get request

--- part4/controllers/users.js
+++ part4/controllers/users.js
@@ -1,8 +1,7 @@
 const bcrypt = require("bcrypt");
 const usersRouter = require("express").Router();
 const User = require("../models/userModel");
-const jwt = require('jsonwebtoken');
-
+const jwt = require("jsonwebtoken");
 
 usersRouter.post("/", async (req, res, next) => {
   //signup end-point
@@ -35,7 +34,6 @@ usersRouter.post("/", async (req, res, next) => {
   }
 });
 
-
 usersRouter.post("/login", async (req, res, next) => {
   //LOGIN METHHOD
   const { username, password } = req.body;
@@ -61,14 +59,30 @@ usersRouter.post("/login", async (req, res, next) => {
   res.status(200).send({ token, username: user.username, name: user.name });
 });
 
-usersRouter.get("/", async(req, res) => {
-    const users =await User.find({}).populate("blogs", {title: 1, author: 1 , url: 1, likes:1});
-    res.json(users);
-})
+usersRouter.get("/", async (req, res) => {
+  const users = await User.find({}).populate("blogs", {
+    title: 1,
+    author: 1,
+    url: 1,
+    likes: 1,
+  });
+  res.json(users);
+});
+
+//find single user
+
+usersRouter.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    console.log(user, "USER FINDBYID");
+    res.status(200).send(user);
+  } catch (error) {
+    res.status(404).send({
+      error: error.message,
+    });
+    next(error);
+  }
+});
 
 module.exports = usersRouter;
-
-
-
-
-
